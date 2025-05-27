619e2ab4229e4169ecf58f87a3d673ce988479e8
RudyHarun99 <rdyhrn@gmail.com>
2025-05-27 19:15:08 +0700

    monorepo using turborepo

--- apps/web-admin/app/lib/trpc.ts
+++ apps/web-admin/app/lib/trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../../../backend/src/trpc/routers';
+import type { AppRouter } from '../../../server/src/trpc/routers';
 
 export const trpc = createTRPCReact<AppRouter>();
